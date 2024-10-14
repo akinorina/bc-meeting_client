f6017fb20ea17f9c329716f8a957bbf90e500431
Akinori Nakata <akinori.nakata@bright-century.com>
2024-10-14 12:46:36 +0900

    ビデオ通話 レイアウト更新

--- src/views/room/IndexView.vue
+++ src/views/room/IndexView.vue
@@ -93,38 +93,84 @@ const checkStatusPeerConn = async () => {
 </script>
 
 <template>
-  <div class="h-screen bg-slate-100">
-    <div class="p-3">
-      <div class="font-bold">Contact Rooms</div>
-      <div class="p-3">
-        <div class="">{{ roomStore.room.room_name }}</div>
-        <div class="">{{ roomStore.room.room_hash }}</div>
+  <div class="h-screen bg-slate-100" v-if="statusEnterRoom === false">
+    <div class="flex">
+      <div class="w-1/2 m-5">
+        <video class="w-full bg-slate-100" :srcObject.prop="webrtcStore.myMediaStream" autoplay muted playsinline></video>
+      </div>
+      <div class="w-1/2 m-5 border rounded-md">
+        <div class="">
+          <div class="p-2 font-bold">Contact Rooms</div>
+          <div class="p-2">
+            <div class="text-xl font-semibold">{{ roomStore.room.room_name }}</div>
+            <div class="">{{ roomStore.room.room_hash }}</div>
+          </div>
+        </div>
+
+        <div class=" flex justify-start">
+          <div class="w-2/6 font-bold p-2">my name.</div>
+          <InputText class="w-4/6 px-3 rounded-md" readonly v-model="webrtcStore.myName" />
+        </div>
+
+        <div class=" flex justify-start">
+          <div class="w-2/6 font-bold p-2">my peer id.</div>
+          <InputText class="w-4/6 px-3 rounded-md" readonly v-model="webrtcStore.myPeerId" />
+        </div>
+        <div class="p-3">
+          <ButtonGeneral class="me-2" @click="router.push({ name: 'index' })">Topへ</ButtonGeneral>
+
+          <ButtonGeneral class="me-2" @click="enterRoom">入室</ButtonGeneral>
+        </div>
       </div>
     </div>
-
-    <div class=" flex justify-start">
-      <div class="w-1/6 font-bold p-2">my name.</div>
-      <InputText class="w-3/6 px-3 rounded-md" readonly v-model="webrtcStore.myName" />
+  </div>
+  <div class="" v-else>
+    <div class="absolute right-2 bottom-10 z-10 w-32 h-16 rounded-md p-2 bg-slate-200">
+      <ButtonGeneral class="" @click="exitRoom">退室</ButtonGeneral>
     </div>
+    <div class="w-screen h-screen flex flex-wrap justify-center">
+      <div
+        class="relative bg-slate-500 border flex items-center"
+        :class="{
+          'w-full': (Object.keys(webrtcStore.peerMedias).length === 0),
+          'w-1/2': (1 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 3),
+          'w-1/3': (4 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 8),
+          'w-1/4': (9 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 15),
+          'w-1/5': (16 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 24),
+          'h-full': (0 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 1),
+          'h-1/2': (2 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 5),
+          'h-1/3': (6 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 11),
+          'h-1/4': (12 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 19),
+          'h-1/5': (20 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 24)
+        }"
+      >
+        <video class="w-full h-full" :srcObject.prop="webrtcStore.myMediaStream" autoplay muted playsinline></video>
+        <div class="absolute right-2 bottom-2">
+          {{ webrtcStore.myPeerId }}
+        </div>
+      </div>
 
-    <div class=" flex justify-start">
-      <div class="w-1/6 font-bold p-2">my peer id.</div>
-      <InputText class="w-3/6 px-3 rounded-md" readonly v-model="webrtcStore.myPeerId" />
-    </div>
-
-    <div class="p-3">
-      <ButtonGeneral class="me-2" @click="router.push({ name: 'index' })">Topページへ</ButtonGeneral>
-
-      <ButtonGeneral class="me-2" @click="enterRoom" v-if="statusEnterRoom === false">入室</ButtonGeneral>
-      <ButtonGeneral class="me-2" @click="exitRoom" v-else>退室</ButtonGeneral>
-    </div>
-
-    <div class="flex flex-wrap">
-      <video class="size-80 bg-slate-100" :srcObject.prop="webrtcStore.myMediaStream" autoplay muted playsinline></video>
-
-      <template v-for="(pm, idx) in webrtcStore.peerMedias" :key="idx">
-        <video class="size-80 bg-slate-100" :srcObject.prop="pm.mediaStream" autoplay muted playsinline></video>
-      </template>
+      <div
+        class="relative bg-slate-500 border flex items-center"
+        :class="{
+          'w-full': (Object.keys(webrtcStore.peerMedias).length === 0),
+          'w-1/2': (1 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 3),
+          'w-1/3': (4 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 8),
+          'w-1/4': (9 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 15),
+          'w-1/5': (16 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 24),
+          'h-full': (0 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 1),
+          'h-1/2': (2 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 5),
+          'h-1/3': (6 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 11),
+          'h-1/4': (12 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 19),
+          'h-1/5': (20 <= Object.keys(webrtcStore.peerMedias).length && Object.keys(webrtcStore.peerMedias).length <= 24)
+        }"
+        v-for="(pm, peerId) in webrtcStore.peerMedias" :key="peerId"
+      >
+        <video class="w-full h-full" :srcObject.prop="pm.mediaStream" autoplay muted playsinline></video>
+        <div class="absolute right-2 bottom-2">
+          {{ peerId }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
