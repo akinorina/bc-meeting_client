f1844d4e5005ba7920c4da070ca5aea7ca6dfbfa
Akinori Nakata <akinori.nakata@bright-century.com>
2024-10-21 12:27:02 +0900

    レイアウト変更を戻し、ビデオ・オーディオ on/off切り替えを適切に実行 修正

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import VccHeader from './components/VccHeader.vue'
-
-const pageName = ref('')
-const noLayoutPageNames = ['room']
-
-const router = useRouter()
-router.beforeEach((to, from, next) => {
-  if (to.name === 'room') {
-    pageName.value = 'room'
-  } else {
-    pageName.value = ''
-  }
-  next()
-})
-
 </script>
 
 <template>
-  <template v-if="noLayoutPageNames.includes(pageName)">
-    <div class="h-screen">
-      <router-view />
-    </div>
-  </template>
-  <template v-else>
-    <div class="h-screen">
-      <VccHeader />
-      <router-view />
-    </div>
-  </template>
+  <div class="h-screen">
+    <VccHeader />
+    <router-view />
+  </div>
 </template>
 
 <style scoped></style>
